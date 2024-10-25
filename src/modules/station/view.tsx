5e3b768d98999f08500646b0716061396cb75c11
Nam HQ <namhq.1989@gmail.com>
2024-10-25 10:10:17 +0700

    feat(web): done ui

--- src/modules/station/view.tsx
+++ src/modules/station/view.tsx
@@ -5,7 +5,8 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet'
-import { ArrowRight } from 'lucide-react'
+import { Badge } from '@/components/ui/badge'
+import { ArrowRight, Heart, Pause } from 'lucide-react'
 
 const side = 'right'
 
@@ -39,13 +40,30 @@ const StationView = () => {
 
 const StationItem = () => {
   return (
-    <div className='flex flex-row gap-4 px-4 py-6 hover:bg-muted hover:cursor-pointer'>
-      <div className='bg-gray-500 rounded-xl h-[100px] aspect-square'></div>
+    <div className='flex flex-row gap-4 px-4 py-4 badge-hover'>
+      <div className='flex flex-col gap-2'>
+        <div className='bg-gray-500 rounded-xl h-[80px] aspect-square'></div>
+        <div className='flex flex-row items-center justify-evenly gap-4'>
+          <Pause strokeWidth={1} className='cursor-pointer' />
+          <Heart
+            strokeWidth={1}
+            className='cursor-pointer'
+            fill='hsl(var(--primary))'
+            stroke='hsl(var(--primary))'
+          />
+        </div>
+      </div>
       <div className='flex flex-col'>
-        <div className='text-lg font-bold'>Station Name</div>
+        <div className='text-base font-bold'>Station Name</div>
         <small className='text-sm text-muted-foreground'>
           Thoughtful, gentle songs, perfect as background music at home or work.
         </small>
+        <div className='flex flex-row flex-wrap gap-2 mt-2'>
+          <Badge variant='outline'>Instrumental</Badge>
+          <Badge variant='outline'>English</Badge>
+          <Badge variant='outline'>Radio</Badge>
+          <Badge variant='outline'>Gaming</Badge>
+        </div>
       </div>
     </div>
   )
