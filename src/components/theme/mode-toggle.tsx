2dcdc741a09e0e18acb365d0a30d3c76e65c64de
Nam Huynh <namhq1989@users.noreply.github.com>
2024-10-25 10:11:45 +0700

    feat(web): add station ui (#3)
    * feat(web): add play button and station information
    
    * feat(web): optimize UI & add list stations
    
    * feat(web): done ui

--- src/components/theme/mode-toggle.tsx
+++ src/components/theme/mode-toggle.tsx
@@ -7,9 +7,9 @@ export function ModeToggle() {
   return (
     <div className='cursor-pointer'>
       {theme === 'dark' ? (
-        <Moon onClick={() => setTheme('light')} />
+        <Moon size={20} onClick={() => setTheme('light')} />
       ) : (
-        <Sun onClick={() => setTheme('dark')} />
+        <Sun size={20} onClick={() => setTheme('dark')} />
       )}
     </div>
   )
--- src/modules/station/preview.tsx
+++ src/modules/station/preview.tsx
@@ -1,7 +1,31 @@
+import { Heart, Pause, VolumeOff } from 'lucide-react'
+import StationView from '@/modules/station/view.tsx'
+import EffectPreview from '@/modules/effect/preview.tsx'
+
 const StationPreview = () => {
   return (
-    <div className='flex w-full h-[150px] bg-gray-500 items-center justify-center'>
-      STATIONS
+    <div className='flex flex-col w-full min-h-[120px]'>
+      <div className='flex flex-grow py-4 px-0'>
+        <div className='flex flex-col gap-2'>
+          <StationView />
+          <small className='text-sm text-muted-foreground'>
+            Thoughtful, gentle songs, perfect as background music at home or
+            work.
+          </small>
+          <div className='flex flex-row items-center justify-between my-6 px-8'>
+            <VolumeOff strokeWidth={1} size={32} className='cursor-pointer' />
+            <Pause strokeWidth={1} size={32} className='cursor-pointer' />
+            <Heart
+              strokeWidth={1}
+              size={32}
+              className='cursor-pointer'
+              fill='hsl(var(--primary))'
+              stroke='hsl(var(--primary))'
+            />
+          </div>
+          <EffectPreview />
+        </div>
+      </div>
     </div>
   )
 }
