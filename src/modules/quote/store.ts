3b9efa6eee2fdf8da1c44e34bd318940268929ed
Nam Huynh <namhq1989@users.noreply.github.com>
2024-11-02 12:20:00 +0700

    feat(web): add quote (#12)

---
+++ src/modules/quote/store.ts
@@ -0,0 +1,49 @@
+import { create } from 'zustand'
+import {
+  IGetQuoteApiResponse,
+  IQuote,
+  IQuoteStore,
+} from '@/modules/quote/types.ts'
+import useHttpStore from '@/modules/http/store.ts'
+
+const FETCH_QUOTE_INTERVAL = 10800000 // 3 hours
+
+const useQuoteStore = create<IQuoteStore>((set) => ({
+  quote: null,
+  getQuote: async () => {
+    chrome.storage.local.get(async (result) => {
+      const now = new Date()
+      const quoteStr: string = result.quote || ''
+      const quoteLastFetchTs: number = result.quoteLastFetchTs || 0
+
+      if (quoteStr && quoteLastFetchTs > 0) {
+        const quoteLastFetchDate = new Date(quoteLastFetchTs)
+        const diff = now.getTime() - quoteLastFetchDate.getTime()
+        if (diff < FETCH_QUOTE_INTERVAL) {
+          const quote = JSON.parse(quoteStr) as IQuote
+          set({ quote })
+          return
+        }
+      }
+
+      const { get: httpGet } = useHttpStore.getState()
+      const response = await httpGet<IGetQuoteApiResponse>(
+        'api/quote/fetch',
+        {},
+      )
+      if (response && response.quote) {
+        set({
+          quote: response.quote,
+        })
+        chrome.storage.local
+          .set({
+            quote: JSON.stringify(response.quote),
+            quoteLastFetchTs: now.getTime(),
+          })
+          .then()
+      }
+    })
+  },
+}))
+
+export default useQuoteStore
