a5ccf213b421287aef8b6cc204b08bb93fb1e7b4
Nam Huynh <namhq1989@users.noreply.github.com>
2024-11-24 22:49:31 +0700

    chore(web): fix subscription plan limitation (#37)

--- src/modules/http/ky.ts
+++ src/modules/http/ky.ts
@@ -1,5 +1,10 @@
 import ky, { KyInstance } from 'ky'
 
+interface INotSuccessResponse {
+  code: string
+  message: string
+}
+
 const http: KyInstance = ky.create({
   prefixUrl: import.meta.env.VITE_API_HOST || 'http://localhost:3000',
   headers: {
@@ -19,7 +24,7 @@ const http: KyInstance = ky.create({
       },
     ],
     afterResponse: [
-      (_, __, response) => {
+      async (_, __, response) => {
         if (response.status === 401) {
           // Handle unauthorized error
           console.error('Unauthorized access - 401')
@@ -27,6 +32,9 @@ const http: KyInstance = ky.create({
         } else if (response.status === 500) {
           // Handle server error
           console.error('Server error - 500')
+        } else if (!response.ok) {
+          const error: INotSuccessResponse = await response.json()
+          throw new Error(error.message)
         }
       },
     ],
