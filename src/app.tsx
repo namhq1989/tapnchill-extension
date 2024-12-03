b9d89318393c85a61008bdc96e4b37c1e2a687c6
Nam Huynh <namhq1989@users.noreply.github.com>
2024-12-03 14:19:18 +0700

    fix(web): pause station did not change state (#47)

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,6 @@ import HomeView from '@/modules/common/home.tsx'
 import { Toaster } from '@/components/ui/toaster.tsx'
 import { ThemeProvider } from '@/components/theme/theme-provider.tsx'
 import useNotificationStore from '@/modules/notification/store.ts'
-import useAmbiencesStore from '@/modules/ambience/store.ts'
 
 chrome.storage.local.get((result) => {
   if (result.isSignedInSuccessfully) {
@@ -32,9 +31,8 @@ chrome.runtime.onMessage.addListener(async (request) => {
       startTime: new Date(),
     })
   } else if (request.type === 'station-is-stopped') {
-    const { ambiences } = useAmbiencesStore.getState()
     setState({
-      isPlaying: ambiences.length > 0,
+      isPlaying: false,
     })
   }
 })
